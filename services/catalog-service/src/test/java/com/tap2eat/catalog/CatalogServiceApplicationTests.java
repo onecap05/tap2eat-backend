017e16a95274189dee7191c3c18c3027a2e14b13
Onecap05 <zs23014095@estudiantes.uv.mx>
2026-03-14 00:20:00 -0600

    added services and sub modules

---
+++ services/catalog-service/src/test/java/com/tap2eat/catalog/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tap2eat.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ services/finance-service/src/main/java/com/tap2eat/finance/FinanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tap2eat.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceServiceApplication.class, args);
+	}
+
+}

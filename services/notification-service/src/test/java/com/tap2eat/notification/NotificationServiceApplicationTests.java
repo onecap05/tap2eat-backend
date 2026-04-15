35a47d64a44572b900536384e05e90830c76aba0
Onecap05 <zs23014095@estudiantes.uv.mx>
2026-04-15 00:54:23 -0600

    feat: configura api gateway base con rutas iniciales

---
+++ services/notification-service/src/test/java/com/tap2eat/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tap2eat.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
